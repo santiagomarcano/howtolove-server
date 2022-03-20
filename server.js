8d288e8bf820090812f72ffa6434aef5241604c3
Santiago Marcano <marcanoblog@gmail.com>
2022-03-19 23:45:03 -0300

    :fire:

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+    // allowedHeaders: ['my-custom-header'],
+    credentials: true
+  }
+})
+const cors = require('cors')
+
+const state = {}
+const messages = []
+
+app.use(cors())
+
+app.get('/state', (req, res) => {
+  res.json(state)
+})
+
+io.on('connection', socket => {
+  socket.on('position', data => {
+    state[data.username] = {
+      ...data
+    }
+    socket.emit('state', state)
+  })
+  socket.on('message', data => {
+    messages.push(data)
+    console.log(socket)
+    socket.emit('chat', messages)
+  })
+  socket.emit('chat', messages)
+  setInterval(() => {
+    socket.emit('chat', messages)
+  }, 1000)
+})
+
+server.listen(4000, () => {
+  console.log('listening on *:4000')
+})
